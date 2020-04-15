45cbcf63f7a90979bbcba7bfd9658cb45c7b02b8
drew-private <andrei.p.neagu@gmail.com>
2020-04-16 00:19:42 +0300

    Finalized part of the code for the conversion from binary to decimal

--- test.js
+++ test.js
@@ -34,7 +34,52 @@
 //
 // console.log(result)
 
+let arr = [2, 3, 5, 6, 7, 5];
+let basePow = 2;
 
-const reversedNum = num => parseFloat(num.toString().split('').reverse().join(''));
+let finalizedArray = function mainCalculation() {
+    //Loop for calculating the exponent
+    let exponentArr = [];
+    let raisedBinary = [];
 
-console.log(reversedNum(43453));
+    for (i = 0; i < arr.length; i++) {
+        exponentArr.push(i);
+        raisedBinary.push(Math.pow(basePow, exponentArr[i]));
+    }
+
+    //Loop for raising 2 to a power
+
+    // for (i = 0; i < arr.length; i++) {
+    //     raisedBinary.push(Math.pow(basePow, exponentArr[i]));
+    // }
+
+    //map the mofo with the new values-- Multiply with 2*
+
+    let newArr = arr.map((x, y) => {
+      return  x * raisedBinary[y];
+    });
+
+    return newArr;
+};
+
+console.log(finalizedArray());
+
+// for (i = 0; i < arr.length; i++) {
+//     newArr.push(i);
+// }
+//
+// console.log(newArr);
+// let newArr = arr.map(function(base, exponent) {
+//    return Math.pow(arr[base], exponentArr[exponent]);
+// });
+// console.log(newArr);
+
+// let newArr = arr.map(x => {
+//         // var display = Math.pow(x, pow);
+//         var display = x**pow;
+//         console.log(pow);
+//         return display;
+// });
+
+
+// // console.log(newArr + ' Asta este rezultatul de la array');
